45e2f15339d394129b9468bcb1d0f3b6a7953ded
jphager2 <jphager2@gmail.com>
2016-05-10 14:10:29 +0200

    using curb

--- basic_api_client.gemspec
+++ basic_api_client.gemspec
@@ -2,7 +2,7 @@ files = ['lib/basic_api_client.rb', 'LICENSE', 'README.md']
 
 Gem::Specification.new do |s|
   s.name        = 'basic_api_client'
-  s.version     = '1.1.0'
+  s.version     = '2.0.0'
 	s.date        = "#{Time.now.strftime("%Y-%m-%d")}"
 	s.homepage    = 'https://github.com/jphager2/basic_api_client'
   s.summary     = 'Example of a basic api client that keeps access-token headers'
@@ -12,6 +12,6 @@ Gem::Specification.new do |s|
   s.files       = files 
   s.license     = 'MIT'
 
-  s.add_runtime_dependency 'faraday'
+  s.add_runtime_dependency 'curb'
   s.add_runtime_dependency 'json'
 end
--- lib/basic_api_client.rb
+++ lib/basic_api_client.rb
@@ -1,77 +1,79 @@
-require 'faraday'
+require 'curb'
 require 'json'
 
 class BasicApiClient
   AUTH_HEADERS = %w{ access-token token-type uid expiry client }.sort
 
   attr_reader :headers
-  attr_accessor :last_response
+  attr_accessor :last_response, :last_return
 
-  def initialize(cn, password, site, auth_url)
+  def initialize(cn, password, site, auth_path)
     @cn = cn
     @password = password
     @site = site
-    @auth_url = auth_url
+    @site.sub!(/\/+$/, '')
+    @auth_url = url_for(auth_path)
   end
 
   def headers
     @headers ||= {}
   end
 
-  def conn
-    @conn ||= begin
-      Faraday.new(url: @site) do |faraday|
-        faraday.request :url_encoded
-        faraday.response :logger
-        faraday.adapter Faraday.default_adapter
-      end
-    end
-  end
-
   def authorize
-    res = conn.post do |req|
-      req.url @auth_url 
-      req.params['cn'] = @cn
-      req.params['password'] = @password
-    end
-
-    handle_response(res)
+    @headers = {}
+    params = { cn: @cn, password: @password }
+    Curl.post(@auth_url, params, &method(:setup_request))
+    last_return
   end
 
-  def get(url, params = {})
+  def get(path, params = {})
     authorize unless auth_headers?
 
-    res = conn.get do |req|
-      req.url url
-      req.params = params.merge(req.params)
-      req.headers = headers
-    end
-
-    handle_response(res)
+    url = url_for(path)
+    Curl.get(url, params, &method(:setup_request))
+    last_return
   end
 
-  def post(url, params = {})
+  def post(path, params = {})
     authorize unless auth_headers?
 
-    res = conn.post do |req|
-      req.url url
-      req.params = params.merge(req.params)
-      req.headers = headers
-    end
-
-    handle_response(res)
+    url = url_for(path)
+    Curl.post(url, params, &method(:setup_request))
+    last_return
   end
 
   private 
-  def handle_response(res)
-    self.last_response = res
-    if res.success?
-      @headers = res.headers.select { |k, v| AUTH_HEADERS.include?(k) }
-      JSON.parse(res.body)
-    else
-      @headers = {}
-      false
-    end
+  def url_for(path)
+    path = path.sub(/^\/+/, '')
+    [@site, path].join('/')
+  end
+
+  def setup_request(req)
+    req.headers = headers
+    req.on_failure &method(:handle_failure)
+    req.on_missing &method(:handle_failure)
+    req.on_success &method(:handle_success)
+  end
+
+  def handle_success(res)
+    @last_response = res
+    @headers = parse_headers(res).select { |name, _| 
+      AUTH_HEADERS.include?(name) }
+    @last_return = JSON.parse(res.body_str)
+  end
+
+  def handle_failure(res, code)
+    @last_response = res
+    @headers = {}
+    @last_return = nil
+  end
+
+  def parse_headers(res)
+    lines = res.header_str.split("\r\n").drop(1)
+    lines.each_with_object({}) { |line, headers| 
+      name, value = line.split(": ")
+      name.downcase!
+      headers[name] = value }
   end
 
   def auth_headers?
